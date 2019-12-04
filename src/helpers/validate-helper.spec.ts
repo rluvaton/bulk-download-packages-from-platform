2acbda6039f9090e7eadde131fdfc0148f9c0f16
Raz Luvaton <rluvaton@gmail,com>
2019-12-04 16:32:51 +0200

    Add tests #5 - add `isNumber` test in `validate-helper` file
    Add option to include NaN or not in the isNumber function

--- src/helpers/validate-helper.spec.ts
+++ src/helpers/validate-helper.spec.ts
@@ -1,7 +1,7 @@
-import {isNil} from './validate-helper';
+import {isNil, isNumber} from './validate-helper';
 
 describe('Validate Helper Tests', () => {
-  describe('isNil Tests', () => {
+  describe('#isNil Tests', () => {
     it('should have the function isNil', () => {
       expect(isNil).toBeDefined();
       expect(typeof isNil).toBe('function');
@@ -75,7 +75,128 @@ describe('Validate Helper Tests', () => {
     });
 
     it('should return false for function argument (truthy argument)', () => {
-      expect(isNil(() => {})).toBe(false);
+      expect(isNil(() => {
+      })).toBe(false);
     });
+  });
+
+  describe('#isNumber Tests', () => {
+
+    it('should have the function isNumber', () => {
+      expect(isNumber).toBeDefined();
+      expect(typeof isNumber).toBe('function');
+    });
+
+    describe('should return true for number argument', () => {
+      it('includeNaN is false', () => {
+        expect(isNumber(5, false)).toBe(true);
+      });
+
+      it('includeNaN is true', () => {
+        expect(isNumber(5, true)).toBe(true);
+      });
+    });
+
+    it('should be called with includeNaN as false when providing only 1 argument', () => {
+      const isNumberMock = jest.fn(isNumber);
+      isNumberMock('arg');
+      expect(isNumberMock).toHaveBeenCalledWith('arg');
+      expect(isNumberMock).toHaveReturnedTimes(1);
+      expect(isNumberMock).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return false for NaN without providing 2nd argument (includeNaN should be false)', () => {
+      expect(isNumber(NaN)).toBe(false);
+    });
+
+    it('should return false for no arguments', () => {
+      // @ts-ignore
+      expect(isNumber()).toBe(false);
+    });
+
+    it('should return false for NaN argument and includeNaN is false', () => {
+      expect(isNumber(NaN, false)).toBe(false);
+    });
+
+    it('should return true for NaN argument and includeNaN is true', () => {
+      expect(isNumber(NaN, true)).toBe(true);
+    });
+
+    describe('should return false for null argument', () => {
+      it('includeNaN is false', () => {
+        expect(isNumber(null, false)).toBe(false);
+      });
+
+      it('includeNaN is true', () => {
+        expect(isNumber(null, true)).toBe(false);
+      });
+    });
+
+    describe('should return false for undefined argument', () => {
+      it('includeNaN is false', () => {
+        expect(isNumber(undefined, false)).toBe(false);
+      });
+      it('includeNaN is true', () => {
+        expect(isNumber(undefined, true)).toBe(false);
+      });
+    });
+
+    describe('should return false for empty string argument', () => {
+      it('includeNaN is false', () => {
+        expect(isNumber('', false)).toBe(false);
+      });
+      it('includeNaN is true', () => {
+        expect(isNumber('', true)).toBe(false);
+      });
+    });
+
+    describe('should return false for Number instance', () => {
+      it('includeNaN is false', () => {
+        // tslint:disable-next-line:no-construct
+        expect(isNumber(new Number(3), false)).toBe(false);
+      });
+
+      it('includeNaN is true', () => {
+        // tslint:disable-next-line:no-construct
+        expect(isNumber(new Number(3), true)).toBe(false);
+      });
+    });
+
+    describe('should return false for empty object argument', () => {
+      it('includeNaN is false', () => {
+        expect(isNumber({}, false)).toBe(false);
+      });
+      it('includeNaN is true', () => {
+        expect(isNumber({}, true)).toBe(false);
+      });
+    });
+
+    describe('should return false for object argument', () => {
+      it('includeNaN is false', () => {
+        expect(isNumber({a: 1}, false)).toBe(false);
+      });
+      it('includeNaN is true', () => {
+        expect(isNumber({a: 1}, true)).toBe(false);
+      });
+    });
+
+    describe('should return false for empty array argument', () => {
+      it('includeNaN is false', () => {
+        expect(isNumber([], false)).toBe(false);
+      });
+      it('includeNaN is true', () => {
+        expect(isNumber([], true)).toBe(false);
+      });
+    });
+
+    describe('should return false for array argument', () => {
+      it('includeNaN is false', () => {
+        expect(isNumber([1], false)).toBe(false);
+      });
+      it('includeNaN is true', () => {
+        expect(isNumber([1], true)).toBe(false);
+      });
+    });
+
   });
 });
--- src/helpers/validate-helper.ts
+++ src/helpers/validate-helper.ts
@@ -11,12 +11,22 @@ export function isNil(value: any): boolean {
 }
 
 /**
- * return if variable is number or not
+ * Return if variable is number or not
  * @param num any variable to check
+ * @param includeNaN To include NaN in the validation
  * @return true if the variable is a number
+ *
+ * @summary
+ * Why does typeof NaN return “number”?
+ * typeof NaN; // "number"
+ * The ECMAScript standard states that Numbers should be IEEE-754 floating point data. This includes Infinity, -Infinity, and also NaN.
+ * By definition, NaN is the return value from operations which have an undefined numerical result.
+ * Hence why, in JavaScript, aside from being part of the global object, it is also part of the Number object: Number.NaN.
+ * ***It is still a numeric data type, but it is undefined as a real number.***
+ * NaN also represents any number outside of the ECMAScript domain of definition.
  */
-export function isNumber(num: any): boolean {
-  return typeof num === 'number';
+export function isNumber(num: any, includeNaN: boolean = false): boolean {
+  return typeof num === 'number' && (includeNaN || !isNaN(num));
 }
 
 /**
